8da673a2a38ca205cdbffa804cf613c970cc01e0
Adrian Merida <a.merida91@gmail.com>
2020-02-07 20:01:09 +0100

    chat model created

---
+++ models/Chat.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const chatSchema = new Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => { //ret => return
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      }
+    }
+  }
+)
+
+const Chat = new mongoose.model('Chat', chatSchema)
+
+module.exports = Chat
